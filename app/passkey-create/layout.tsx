a66591a783dcfeaa4085cc7e676be384676f7e6a
k-xo <alkassimk@gmail.com>
2024-03-16 14:57:05 +0100

    fix: build

--- app/passkey-create/layout.tsx
+++ app/passkey-create/layout.tsx
@@ -1,9 +1,12 @@
 import '../globals.css';
+import { Suspense } from 'react';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
+    <Suspense>
+      <html lang="en">
+        <body>{children}</body>
+      </html>
+    </Suspense>
   );
 }
