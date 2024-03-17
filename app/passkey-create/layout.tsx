9815bc383ea44a53bc646eabe996fbec97aa9ecc
k-xo <alkassimk@gmail.com>
2024-03-17 05:24:27 +0100

    fix: css

--- app/passkey-create/layout.tsx
+++ app/passkey-create/layout.tsx
@@ -5,7 +5,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <Suspense>
       <html lang="en">
-        <body>{children}</body>
+        <body className={`bg-black text-white`}>{children}</body>
       </html>
     </Suspense>
   );
