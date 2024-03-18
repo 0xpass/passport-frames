09435d9f6dfb2da318273f720761dca7e23c842e
k-xo <alkassimk@gmail.com>
2024-03-18 10:21:42 +0100

    chore: validate likes and recasts

--- app/api/eligibility/route.ts
+++ app/api/eligibility/route.ts
@@ -1,10 +1,15 @@
-import { FrameRequest, getFrameHtmlResponse } from '@coinbase/onchainkit';
+import { FrameRequest, getFrameHtmlResponse, getFrameMessage } from '@coinbase/onchainkit';
 import { kv } from '@vercel/kv';
 import { NextResponse } from 'next/server';
 import { NEXT_PUBLIC_URL } from '../../config';
 
 export async function POST(req: Request) {
   const body: FrameRequest = await req.json();
+  const { isValid, message } = await getFrameMessage(body, { neynarApiKey: 'NEYNAR_ONCHAIN_KIT' });
+
+  if (!isValid) {
+    throw new Error('Invalid frame request');
+  }
 
   const { untrustedData } = body;
   const options = { method: 'GET', headers: { Authorization: `Bearer ${process.env.PINATA_API}` } };
@@ -18,8 +23,10 @@ export async function POST(req: Request) {
   const username = userData.username;
 
   const address = await kv.get(`user:${username}`);
+  const liked = message?.liked;
+  const recasted = message?.recasted;
 
-  if (address) {
+  if (address && liked && recasted) {
     return new NextResponse(
       getFrameHtmlResponse({
         image: {
